826e7a3fe565a93951a2f51402b2ebbd0c9ed50a
benjflowers <benjaminjflowers@gmail.com>
2021-06-04 17:14:40 -0500

    fixed issue where user could navigate to login or sign up after logging on

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import axios from "axios";
 // Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userActions";
 //Components
 import Navbar from "./components/Navbar";
 import AuthRoute from "./util/AuthRoute";
@@ -19,18 +21,17 @@ import signup from "./pages/signup";
 
 const theme = createMuiTheme(themeObject);
 
-let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 > Date.now()) {
+    store.dispatch(logoutUser());
     window.location.href = "/login";
-    authenticated = false;
   } else {
+    store.dispatch({ type: SET_AUTHENTICATED });
     axios.defaults.headers.common["Authorization"] = token;
-    authenticated = true;
+    store.dispatch(getUserData());
   }
-  console.log(decodedToken);
 }
 
 class App extends Component {
@@ -43,18 +44,8 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={home}></Route>
-                <AuthRoute
-                  exact
-                  path="/login"
-                  component={login}
-                  authenticated={authenticated}
-                />
-                <AuthRoute
-                  exact
-                  path="/signup"
-                  component={signup}
-                  authenticated={authenticated}
-                />
+                <AuthRoute exact path="/login" component={login} />
+                <AuthRoute exact path="/signup" component={signup} />
               </Switch>
             </div>
           </Router>
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -5,6 +5,7 @@ import {
   CLEAR_ERRORS,
   LOADING_UI,
   LOADING_USER,
+  SET_UNAUTHENTICATED,
 } from "../types";
 
 export const loginUser = (userData, history) => (dispatch) => {
@@ -23,6 +24,32 @@ export const loginUser = (userData, history) => (dispatch) => {
         payload: err.response.data,
       });
     });
+};
+
+export const signupUser = (newUserData, history) => (dispatch) => {
+  dispatch({ type: LOADING_UI });
+  axios
+    .post("/signup", newUserData)
+    .then((res) => {
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch((err) => {
+      dispatch({
+        type: SET_ERRORS,
+        payload: err.response.data,
+      });
+    });
+};
+
+export const logoutUser = () => (dispatch) => {
+  localStorage.removeItem("FBIdToken");
+  delete axios.defaults.headers.common["Authorization"];
+  dispatch({
+    type: SET_UNAUTHENTICATED,
+  });
 };
 
 export const getUserData = () => (dispatch) => {
